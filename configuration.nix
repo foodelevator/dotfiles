7bcb861a6db76befc33f78c3066cbc26dd296830
mathiasmagnusson <mathiasmagnussons@gmail.com>
2022-07-26 22:30:21 +0200

    Add nixos flake config

---
+++ configuration.nix
@@ -0,0 +1,159 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Stockholm";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.utf8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "sv_SE.utf8";
+    LC_IDENTIFICATION = "sv_SE.utf8";
+    LC_MEASUREMENT = "sv_SE.utf8";
+    LC_MONETARY = "sv_SE.utf8";
+    LC_NAME = "sv_SE.utf8";
+    LC_NUMERIC = "sv_SE.utf8";
+    LC_PAPER = "sv_SE.utf8";
+    LC_TELEPHONE = "sv_SE.utf8";
+    LC_TIME = "sv_SE.utf8";
+  };
+
+  hardware.i2c.enable = true;
+  hardware.opengl.enable = true;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbVariant = "altgr-intl";
+    videoDrivers = [ "nvidia" ];
+    displayManager.setupCommands = ''
+      ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-1 --mode 1920x1080 --rate 60 --output DP-0 --mode 1920x1080 --rate 166 --right-of HDMI-1
+    '';
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+    };
+    libinput = {
+      enable = true;
+      mouse = {
+        accelProfile = "flat";
+      };
+    };
+    excludePackages = [ pkgs.xterm ];
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mathias = {
+    isNormalUser = true;
+    description = "mathias";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
+    packages = with pkgs; [
+      alacritty
+      kitty
+      feh
+      lf
+      xclip
+      tree-sitter
+      ripgrep
+      fd
+
+      zig
+      rustup
+      go
+
+      i3blocks
+
+      firefox
+      discord
+      spotify
+      openrgb
+      pavucontrol
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = with pkgs; [
+    neovim
+    git
+    fish
+    gcc
+  ];
+
+  environment.variables = rec {
+    CARGO_HOME = "\${HOME}/.local/share/cargo";
+    RUSTUP_HOME = "\${HOME}/.local/share/rustup";
+    VISUAL = "nvim";
+    EDITOR = "nvim";
+  };
+
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+}
---
+++ flake.nix
@@ -0,0 +1,20 @@
+# Resources
+# https://www.youtube.com/watch?v=AGVXJ-TIv3Y
+# https://serokell.io/blog/practical-nix-flakes
+
+{
+  description = "System config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      pingupc = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
