10c22b67710e6344583b1cb1bf5cd99b9189c684
mathiasmagnusson <mathiasmagnussons@gmail.com>
2022-08-16 21:29:57 +0200

    Add taplop to syncthing

--- programs/syncthing/default.nix
+++ programs/syncthing/default.nix
@@ -8,6 +8,7 @@
 
     devices = {
       chonk.id = "N6CY7HR-KBEBJY5-ZMS3U2B-TMJY4H7-MVW47VN-UK3YMWT-GLWR7PR-NJT5PQI";
+      taplop.id = "JOOVGP7-BNR6OTA-H5W53FT-EUY6O3T-KZSP5MT-EFZR7QL-WOZA6RT-ANKZOQ3";
     };
 
     folders = {
