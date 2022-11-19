650ab97fc286da020efe913527f9242d4b4186a8
mathiasmagnusson <mathiasmagnussons@gmail.com>
2022-11-19 14:12:29 +0100

    Moar stuaf

--- hosts/chonk/configuration.nix
+++ hosts/chonk/configuration.nix
@@ -44,9 +44,9 @@
       displayManager = {
       lightdm.enable = true;
       setupCommands = "${pkgs.xorg.xrandr}/bin/xrandr " +
-                      "--output HDMI-1 --mode 1920x1080 --rate 60 " +
-                      "--output DP-0 --mode 1920x1080 --rate 166 --right-of HDMI-1 " +
-                      "--output DP-2 --mode 1920x1080 --rate 144 --right-of DP-0";
+                        "--output HDMI-1         --mode 1920x1080 --rate 60  --pos 0x0 " +
+                        "--output DP-0 --primary --mode 1920x1080 --rate 166 --pos 1920x0 " +
+                        "--output DP-2           --mode 1920x1080 --rate 144 --pos 3840x0";
     };
     windowManager.i3 = {
       enable = true;
