501458a26be17042ff1cca60f87e84eb8fadc264
mathiasmagnusson <mathiasmagnussons@gmail.com>
2022-11-21 19:45:00 +0100

    Improve npm setup

--- programs/node/default.nix
+++ programs/node/default.nix
@@ -1,8 +1,13 @@
 { config, pkgs, ... }:
 {
-  home.shellAliases = {
-    npmg = "npm --prefix=\"$HOME/.local/share/npm\"";
+  home.sessionVariables = {
+    npm_config_prefix = "\${HOME}/.local/share/npm";
+    npm_config_cache="\${HOME}/.cache/npm";
   };
+
+  home.sessionPath = [
+    "$HOME/.local/share/npm/bin"
+  ];
 
   home.packages = with pkgs; [
     nodejs
