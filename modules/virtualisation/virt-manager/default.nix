4c2a4eefb2753d9bbf80417a416868a1fc632c25
mathiasmagnusson <mathias@magnusson.space>
2023-03-07 13:44:20 +0100

    Add usb redirection to vms

--- modules/virtualisation/virt-manager/default.nix
+++ modules/virtualisation/virt-manager/default.nix
@@ -10,6 +10,7 @@ in
 
   config = mkIf cfg.enable {
     virtualisation.libvirtd.enable = true;
+    virtualisation.spiceUSBRedirection.enable = true;
 
     environment.systemPackages = with pkgs; [
       virt-manager
