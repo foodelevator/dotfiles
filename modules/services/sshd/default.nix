69995ed909f88c0b6185a84357efd048f4fce11e
mathiasmagnusson <mathias@magnusson.space>
2023-03-27 19:19:28 +0200

    Add yubikey ssh key to server

--- modules/services/sshd/default.nix
+++ modules/services/sshd/default.nix
@@ -12,6 +12,7 @@ in
     elevate.user.sshKeys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPC69ml72mqbn7L3QkpsCJuWdrKFYFNd0MaS5xERbuSF mathias@pingu-arch"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAO88bhtrgWXg4zY8jIAVqzyHKa+PNJRpLbyk86y4Glc mathias@taplop"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEdUe7mxGdV/Q37RKndPzDHisFb7q/xm+L97jcGluSDOA8MGt/+wTxpyGxfyEqaMvwV2bakaMVHTB3711dDu5kE= black yubikey"
     ];
     services.openssh = {
       enable = true;
