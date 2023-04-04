1e3d19911e61ae909461d4298b208bae546ef032
mathiasmagnusson <mathias@magnusson.space>
2023-04-04 16:02:38 +0200

    Improve vim completion keymaps

--- programs/nvim/lua/completion.lua
+++ programs/nvim/lua/completion.lua
@@ -9,6 +9,8 @@ cmp.setup {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<C-l>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
