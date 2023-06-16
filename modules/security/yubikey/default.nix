fbbb71c450b644830c85065f0f5ff8bf137d3a71
Mathias Magnusson <mathias@magnusson.space>
2023-06-16 14:06:12 +0200

    Add graphical prompt for copying passwords

--- modules/security/yubikey/default.nix
+++ modules/security/yubikey/default.nix
@@ -4,6 +4,24 @@ let
   cfg = config.elevate.security.yubikey;
 
   dir = "$HOME/.local/share/passage";
+  store = "${dir}/store";
+
+  prompt = pkgs.writeShellScriptBin "rofi-passage-prompt" ''
+    escstore=$(echo "${store}" | sed \
+      -e 's/\//\\\//g' \
+      -e 's/\./\\./g')
+    passwords=$(find "${store}" -name \*.age | \
+      sed -E "s/$escstore\/(.*)\.age/\1/")
+
+    password=$(printf '%s\n' "''${passwords[@]}" | \
+      ${pkgs.rofi}/bin/rofi -dmenu -sort -matching fuzzy)
+
+    [[ -n $password ]] || exit
+
+    out=$(mktemp)
+    passage -c "$password" >$out 2>$out || rofi -e "$(cat $out)"
+    rm $out
+  '';
 in
 {
   options.elevate.security.yubikey = {
@@ -27,10 +45,12 @@ in
       age-plugin-yubikey
 
       (passage.overrideAttrs (_: { extraPath = makeBinPath [age git xclip tree qrencode]; }))
+
+      prompt
     ];
 
     environment.variables = {
-      PASSAGE_DIR = "${dir}/store";
+      PASSAGE_DIR = store;
       PASSAGE_IDENTITIES_FILE = "${dir}/identities";
       PASSAGE_EXTENSIONS_DIR = "${dir}/extensions";
     };
