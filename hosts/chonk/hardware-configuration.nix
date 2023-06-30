41edd5e54e055935e66a7b2ee03942fb276131e7
Mathias Magnusson <mathias@magnusson.space>
2023-06-30 21:32:05 +0200

    Add hostplatform option

--- hosts/chonk/hardware-configuration.nix
+++ hosts/chonk/hardware-configuration.nix
@@ -29,5 +29,6 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
 
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
