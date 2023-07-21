064331a7e78dccd5d9c9306f9e8e88821c45b63c
Mathias Magnusson <mathias@magnusson.space>
2023-07-21 21:53:53 +0200

    Add signal desktop app

--- modules/sets/desktop/default.nix
+++ modules/sets/desktop/default.nix
@@ -10,7 +10,7 @@ in
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      firefox ungoogled-chromium slack spotify obsidian zoom-us gimp
+      firefox ungoogled-chromium slack spotify obsidian zoom-us gimp signal-desktop
       (discord.override { nss = nss_latest; }) # override needed to open links
     ];
 
