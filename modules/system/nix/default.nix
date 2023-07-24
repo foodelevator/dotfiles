87c5a80384a9112611aff37f32ae9c041a4231e6
Mathias Magnusson <mathias@magnusson.space>
2023-07-24 20:55:01 +0200

    Change automatic gc

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -18,13 +18,12 @@ in
 
     nix.settings.trusted-users = [ config.elevate.user.name ];
 
-    nix.gc.automatic = true;
-    nix.gc.options = "--delete-older-than 7d";
-    nix.gc.dates = "weekly";
-
     nix.package = pkgs.nixFlakes;
     nix.extraOptions = ''
       experimental-features = nix-command flakes
+
+      min-free = ${toString (100 * 1024 * 1024)}
+      max-free = ${toString (1024 * 1024 * 1024)}
     '';
   };
 }
