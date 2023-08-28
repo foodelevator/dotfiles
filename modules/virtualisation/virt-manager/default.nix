2197cf9a527d59030f7c887a8c13d50534596f2e
Mathias Magnusson <mathias@magnusson.space>
2023-08-28 19:05:32 +0200

    Add dconf to virt-manager config

--- modules/virtualisation/virt-manager/default.nix
+++ modules/virtualisation/virt-manager/default.nix
@@ -15,6 +15,7 @@ in
       virtualisation.libvirtd.qemu.package = pkgs.qemu_kvm;
       virtualisation.spiceUSBRedirection.enable = true;
       virtualisation.libvirtd.qemu.swtpm.enable = true;
+      programs.dconf.enable = true; # Makes virt-manager remeber the last connection.
 
       environment.systemPackages = with pkgs; [
         virt-manager
