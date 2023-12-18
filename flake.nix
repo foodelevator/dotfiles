1f1256274def834bb4448ad89ce17a76fc649519
Mathias Magnusson <mathias@magnusson.space>
2023-12-18 20:02:00 +0100

    Remove deploy-rs
    Flake lock file updates:
    
    • Removed input 'deploy-rs'
    • Removed input 'deploy-rs/flake-compat'
    • Removed input 'deploy-rs/nixpkgs'
    • Removed input 'deploy-rs/utils'
    • Removed input 'deploy-rs/utils/systems'

--- flake.nix
+++ flake.nix
@@ -4,12 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    deploy-rs.url = "github:serokell/deploy-rs";
-    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, unstable, deploy-rs } @ inputs:
+  outputs = { self, nixpkgs, unstable } @ inputs:
     let
       system = "x86_64-linux";
 
@@ -41,17 +38,6 @@
 
       packages.${system} = {
         inherit (pkgs) binary-ninja dyalog;
-        inherit (deploy-rs.packages.${system}) deploy-rs;
       };
-
-      deploy.nodes = builtins.mapAttrs
-        (hostname: cfg: {
-          hostname = "old";
-          profiles.system.path = deploy-rs.lib.x86_64-linux.activate.nixos cfg;
-          profiles.system.user = "root";
-        })
-        (pkgs.lib.filterAttrs
-          (_: { config, ... }: config.elevate.archetypes.server.enable)
-          self.nixosConfigurations);
     };
 }
