0b61dfb49a97d5a23ccef0dd4ea6f8d76f75669a
Mathias Magnusson <mathias@magnusson.space>
2024-01-14 14:48:59 +0100

    Add audacity

--- modules/sets/desktop/default.nix
+++ modules/sets/desktop/default.nix
@@ -10,7 +10,7 @@ in
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      firefox ungoogled-chromium slack spotify obsidian zoom-us gimp signal-desktop zulip
+      firefox ungoogled-chromium slack spotify obsidian zoom-us gimp signal-desktop zulip audacity
       (discord.override { nss = nss_latest; }) # override needed to open links
 
       mpv feh
